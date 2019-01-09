3b3f8e3998a2f1feeecabbedf2e8e79073fb7919
Unknown <rafik.laredj@laposte.net>
2019-01-09 23:36:59 +0100

    Correctifs des tests

--- src/test/java/com/myapp/tests/InitRestTemplateTest.java
+++ src/test/java/com/myapp/tests/InitRestTemplateTest.java
@@ -12,31 +12,14 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.junit.Assert.fail;
 import static org.springframework.http.HttpMethod.GET;
 
-public class AnswerRestTemplateTest extends AbstractPackagerViewTest {
+public class InitRestTemplateTest extends AbstractPackagerViewTest {
 
     @Test
-    public void foundAnswer(){
-        Long expectedId = 1L;
+    public void init(){
         ResponseEntity<Answer> response = restTemplate.exchange(
-                baseUrl + "/answers/" + expectedId,
+                baseUrl + "/init/",
         GET, null, Answer.class);
         Assertions.assertThat(response).isNotNull();
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(response.getBody().getId()).isEqualTo(expectedId);
-    }
-
-    @Test
-    public void answerNotFound() {
-        try {
-            restTemplate.exchange(
-                    baseUrl + "/answers/"+ (MAX_ELEMENT + 1),
-            GET,
-            null,
-            Answer.class);
-            fail("Should throw 404 error");
-        } catch (HttpClientErrorException e){
-            Assertions.assertThat(e).isNotNull();
-            Assertions.assertThat(e.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        }
     }
 }
